32322bbb9f873c8d62e2aa0ec22507084af74239
SAAD_KHAN <saad.khan@zortik.com>
2023-08-05 14:24:00 +0500

    done

--- client/GoogleDoc-Clone/src/Components/TextEditor.tsx
+++ client/GoogleDoc-Clone/src/Components/TextEditor.tsx
@@ -1,7 +1,7 @@
 import  { useCallback, useEffect, useState } from 'react';
-import Quill from 'quill';
+import Quill ,{ DeltaStatic } from 'quill';
 import 'quill/dist/quill.snow.css';
-import { io } from 'socket.io-client';
+import { io,  Socket } from 'socket.io-client';
 import { useParams } from 'react-router-dom';
 const SAVE_DOCUMENT = 2000;
 
@@ -18,10 +18,10 @@ const TOOLBAR_OPTIONS = [
   ]
   
 function TextEditor() {
-    const [socket, setSocket] = useState();
-    const [quil, setQuil] = useState();
-    const { id: documentId } = useParams();
-    console.log('ddd', documentId);
+    const [socket, setSocket] = useState<Socket | null>(null); // Specify Socket type
+    const [quil, setQuil] = useState<Quill | null>(null); // Specify Quill type
+    const { id: documentId } = useParams<{ id: string }>(); // Specify parameter type
+
 
     useEffect(() => {
         
@@ -61,7 +61,7 @@ function TextEditor() {
 
     useEffect(() => {
         if (socket == null || quil == null) return
-        const handler = (delta) => {
+        const handler = (delta: DeltaStatic) => {
             quil.updateContents(delta);
         }
 
@@ -74,7 +74,7 @@ function TextEditor() {
 
     useEffect(() => {
         if (socket == null || quil == null) return
-        const handler = (delta, oldDelta, source) => {
+        const handler = (delta: DeltaStatic, oldDelta: DeltaStatic, source: string) => { // Add type annotations to delta and source
             if (source != 'user') return
             socket.emit("send-changes", delta);
         }
@@ -89,7 +89,7 @@ function TextEditor() {
 
     useEffect(() => {
         if (socket == null || quil == null) return
-        const handler = (delta, oldDelta, source) => {
+        const handler = (delta: DeltaStatic, oldDelta: DeltaStatic, source: string) => { // Add type annotations to delta and source
             if (source != 'user') return
             socket.emit("send-changes", delta);
         }
@@ -103,22 +103,22 @@ function TextEditor() {
     }, [socket, quil]);
 
 
-  const wrapperRef = useCallback((wrapper :HTMLDivElement) => {
-    if (!wrapper) return;
-
-    wrapper.innerHTML = '';
-    const editor = document.createElement('div');
-      wrapper.append(editor);
-      
-      const q = new Quill(editor, {
+    const wrapperRef = useCallback((wrapper: HTMLDivElement | null) => {
+        if (!wrapper) return;
+    
+        wrapper.innerHTML = '';
+        const editor = document.createElement('div');
+        wrapper.append(editor);
+    
+        const q = new Quill(editor, {
           theme: 'snow',
           modules: { toolbar: TOOLBAR_OPTIONS },
-      });
-
-      q.disable();
-      q.setText('Loading');
-      setQuil(q);
-  }, []);
+        });
+    
+        q.disable();
+        q.setText('Loading');
+        setQuil(q);
+      }, []);
 
   return <div className="container" ref={wrapperRef}>TextEditor</div>;
 }
